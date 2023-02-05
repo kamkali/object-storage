102c28736fa1d276d945659f2f199189cc5bd4c1
kamkali <kamkalis@gmail.com>
2023-02-05 18:58:55 +0100

    feat: implement simple load balancer

--- internal/domain/load_balancer.go
+++ internal/domain/load_balancer.go
@@ -1,6 +1,12 @@
 package domain
 
+import (
+	"context"
+)
+
 type LoadBalancer interface {
+	GetNext(ctx context.Context) (StorageNode, error)
+	ReBalance(ctx context.Context, nodes []StorageNode) error
 }
 
 //go:generate mockery --name=LoadBalancer
